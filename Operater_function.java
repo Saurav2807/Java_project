75cd5b98769517a6aa90698fc234af796ebe3b96
SAURAV KUMAR <107478139+Saurav2807@users.noreply.github.com>
2022-06-20 12:50:37 +0530

    Update Operater_function.java

--- Operater_function.java
+++ Operater_function.java
@@ -6,20 +6,19 @@ public class Operater_function {
 	 accept();
 		}
 		public static void accept() {
-		
-		int num1,num2,choice;
+		int num1,num2;
+		int i;
+		Scanner in =new Scanner(System.in);
+		System.out.println("Enter your Choice with number: \t1:Addition \t2:Subtraction \t3:Multiplication  \t4:Devision \t5:Module");
+		i=in.nextInt();
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter Your 1st No:-");
 		num1=sc.nextInt();
 		System.out.println("Enter Your 2nd No:-");
 		num2=sc.nextInt();
-		Arthmatic_operator(num1,num2);
+		Arthmatic_operator(num1,num2,i);
 		}
-		public static void Arthmatic_operator(int num1,int num2) {
-			int choice,i;
-			Scanner in =new Scanner(System.in);
-			System.out.println("Enter your Choice \n1:Addition \n2:Multiplication \n3:Subtraction \n4:Devision \n5:Module");
-			i=in.nextInt();
+		public static void Arthmatic_operator(int num1,int num2, int i) {
 			int add;
 			add=num1+num2;
 			int sub;
@@ -33,48 +32,48 @@ public class Operater_function {
 		  
 		switch(i) {
 		case 1:
-			System.out.println("Addition of these NO:-"+add);
+			System.out.println("Addition of these NO:- "+add);
 			if (add%2==0) {
-				System.out.println("This is even NO:-"+add);
+				System.out.println("This is even NO:- "+add);
 			}
 			else {
-				System.out.println("This is Odd NO:-"+add);
-			}
-			break;
-		case 2:
-			System.out.println("Multiplication of Two NO:-"+mul);
-			if (mul%2==0) {
-				System.out.println("This is even NO:-"+mul);
-			}
-			else {
-				System.out.println("This is Odd NO:-"+mul);
+				System.out.println("This is Odd NO:- "+add);
 			}
 			break;
 		case 3:
-			System.out.println("Subtraction of Two NO:-"+sub);
-			if (sub%2==0) {
-			System.out.println("This is even NO:-"+sub);
+			System.out.println("Multiplication of Two NO:- "+mul);
+			if (mul%2==0) {
+				System.out.println("This is even NO:- "+mul);
 			}
 			else {
-			System.out.println("This is Odd NO:-"+sub);
+				System.out.println("This is Odd NO:- "+mul);
+			}
+			break;
+		case 2:
+			System.out.println("Subtraction of Two NO:- "+sub);
+			if (sub%2==0) {
+			System.out.println("This is even NO:- "+sub);
+			}
+			else {
+			System.out.println("This is Odd NO:- "+sub);
 			}
 			break;
 		case 4:
-			System.out.println("Devision of Two NO:-"+dev);
+			System.out.println("Devision of Two NO:- "+dev);
 			if (dev%2==0) {
-			System.out.println("This is even NO:-"+dev);
+			System.out.println("This is even NO:- "+dev);
 			}
 			else {
-			System.out.println("This is Odd NO:-"+dev);
+			System.out.println("This is Odd NO:- "+dev);
 			}
 			break;
 		case 5:
-			System.out.println("Module of Two NO:-"+mod);
+			System.out.println("Module of Two NO:- "+mod);
 			if (mod%2==0) {
-			System.out.println("This is even NO:-"+mod);
+			System.out.println("This is even NO:- "+mod);
 			}
 			else {
-			System.out.println("This is Odd NO:-"+mod);
+			System.out.println("This is Odd NO:- "+mod);
 			}
 			break;
 		}
